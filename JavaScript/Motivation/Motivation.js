36ec48c10509bb3668c8fde34b0848412b038361
MustafaKH96 <74243634+MustafaKh96@users.noreply.github.com>
2022-02-16 23:31:09 +0100

    JS

---
+++ JavaScript/Motivation/Motivation.js
@@ -0,0 +1,37 @@
+// 1) Definition von JS: 
+//  ist eine Skriptsprache, die ursprünglich 1995 ((für dynamisches 
+//  HTML)) in Webbrowsern entwickelt wurde, um Benutzerinteraktionen 
+//  auszuwerten, Inhalte zu verändern, nachzuladen oder zu generieren 
+//  und so die Möglichkeiten von HTML zu erweitern.
+
+//---------------------------
+
+// JavaScript kann HTML-Inhalte ändern
+// z.B: document.getElementById("demo").innerHTML = "Hello Welt";
+
+// JavaScript kann HTML-Attributwerte ändern
+// z.B: document.getElementById('myImage').src='...';
+
+// JavaScript kann HTML-Stile ändern bzw. erstellen (CSS)
+// z.B: document.getElementById("demo").style.fontSize = "35px";
+
+// JavaScript kann HTML-Elemente ausblenden (Durch display)
+// z.B: document.getElementById("demo").style.display = "none";
+
+// JavaScript kann HTML-Elemente anzeigen (Durch display)
+// z.B: document.getElementById("demo").style.display = "block";
+
+// JavaScript kann HTML-Elemente erstellen.
+// z.B: var log = document.createElement('div');
+//      document.body.appendChild(log);
+
+// usw.
+
+//---------------------------
+
+// In HTML wird JavaScript-Code zwischen den Tags <script>
+// und </script> sowohl in <head> als auch in <body> eingefügt.
+
+// Externes JavaScript:
+// Skripte können auch in externen Dateien platziert werden. (.js)
+// In HTML: <script src="myScript.js"></script>
